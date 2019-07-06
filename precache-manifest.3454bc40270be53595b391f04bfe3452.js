12df7d4250a9929f3d0587e561dee75c85afe2af
Greg Dick <greg.dick@hudl.com>
2019-07-06 09:32:20 -0500

    Updates

---
+++ precache-manifest.3454bc40270be53595b391f04bfe3452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c57bebecd45db77a93eeb090edeb15",
+    "url": "/fg-flashcards/index.html"
+  },
+  {
+    "revision": "756ff17c2b36b32884d0",
+    "url": "/fg-flashcards/static/css/main.0609db08.chunk.css"
+  },
+  {
+    "revision": "3ce93e94390071cb94a7",
+    "url": "/fg-flashcards/static/js/2.270be295.chunk.js"
+  },
+  {
+    "revision": "756ff17c2b36b32884d0",
+    "url": "/fg-flashcards/static/js/main.62ab7bd1.chunk.js"
+  },
+  {
+    "revision": "051071631abdc5c7ddfb",
+    "url": "/fg-flashcards/static/js/runtime~main.8845177f.js"
+  }
+]);
