5d89ee3eb46c9686add16b10580d95822932a112
Greg Dick <gregdick1@gmail.com>
2019-07-06 23:41:37 -0500

    Updates

--- precache-manifest.f3b43adf45dc956504ed62a707961d4c.js
+++ precache-manifest.f3b43adf45dc956504ed62a707961d4c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "db3b9be674a43b7a2bae9ff926f5142c",
+    "revision": "c85a415624c7fddcee5acded5a985911",
     "url": "/fg-flashcards/index.html"
   },
   {
-    "revision": "8d696a68ceefad54ba73",
+    "revision": "f4a605c2ab6778eca8a7",
     "url": "/fg-flashcards/static/css/main.fc62ec45.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fg-flashcards/static/js/2.270be295.chunk.js"
   },
   {
-    "revision": "8d696a68ceefad54ba73",
-    "url": "/fg-flashcards/static/js/main.7141a9d4.chunk.js"
+    "revision": "f4a605c2ab6778eca8a7",
+    "url": "/fg-flashcards/static/js/main.268aae10.chunk.js"
   },
   {
     "revision": "051071631abdc5c7ddfb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fg-flashcards/precache-manifest.114e65834b3d788d87cb76b7e94c4d45.js"
+  "/fg-flashcards/precache-manifest.f3b43adf45dc956504ed62a707961d4c.js"
 );
 
 self.addEventListener('message', (event) => {
